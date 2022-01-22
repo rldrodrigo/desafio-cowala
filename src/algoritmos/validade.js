a48b8672fcd16d1d5ec7ab2b0b096a3bfa55403c
Rodrigo Lima <rodrigo.lima1945@gmail.com>
2022-01-22 11:26:37 -0300

    Update validade.js

---
+++ src/algoritmos/validade.js
@@ -0,0 +1,19 @@
+function validade(data, validade) {
+
+    dataValidade = new Date(data)
+    dataHoje = new Date()
+    //Converter a string para um valor inteiro
+
+    let diaValidade = parseInt(validade.replace('d', ''))
+    //Adiciona o valor da validade
+    dataValidade.setDate(dataValidade.getDate() + diaValidade)
+
+    if (dataHoje > dataValidade)
+        return false
+    else
+        return true
+}
+
+
+console.log(validade("2021-11-17T20:40:09.503Z", "10d"))
+console.log(validade("2021-12-10T00:00:00.000Z", "180d"))
