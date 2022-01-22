ff67e5ea92056cb7d8749d75213b1eaa132fe9ef
Rodrigo Lima <rodrigo.lima1945@gmail.com>
2022-01-22 11:05:56 -0300

    Update classificador.js

--- src/algoritmos/classificador.js
+++ src/algoritmos/classificador.js
@@ -1,17 +1,43 @@
 function classificador(listaObj) {
-    console.log(obj)
+    var souEu = []
+    var responsaveis = []
+    var usuarios = []
+    for (let key in obj) {
+        if (obj[key].souEu === true) {
+            souEu.push(obj[key])
+        } else if (obj[key].responsavel === true) {
+            responsaveis.push(obj[key])
+        } else {
+            usuarios.push(obj[key])
+        }
+    }
+    responsaveis.sort(function (x, y) {
+        let a = x.nome.toUpperCase(),
+            b = y.nome.toUpperCase();
+        return a == b ? 0 : a > b ? 1 : -1
+    })
+    usuarios.sort(function (x, y) {
+        let a = x.nome.toUpperCase(),
+            b = y.nome.toUpperCase();
+        return a == b ? 0 : a > b ? 1 : -1
+    })
+    var array = [...souEu, ...responsaveis, ...usuarios]
+    return array
 }
+// Para testar a função
 let obj = [
+    { souEu: false, responsavel: false, nome: 'Pessoa 2' },
     { souEu: true, responsavel: false, nome: 'João' },
     { souEu: false, responsavel: true, nome: 'Maria' },
     { souEu: false, responsavel: false, nome: 'José' },
     { souEu: false, responsavel: false, nome: 'Fernando' },
     { souEu: false, responsavel: false, nome: 'Antônio' },
+    { souEu: false, responsavel: false, nome: 'Pessoa 4' },
     { souEu: false, responsavel: true, nome: 'Glória' },
     { souEu: false, responsavel: true, nome: 'Nilza' },
-    { souEu: false, responsavel: false, nome: 'Pessoa 2' },
     { souEu: false, responsavel: false, nome: 'Pessoa 3' },
-    { souEu: false, responsavel: false, nome: 'Pessoa 4' },
+
 ]
-console.log(obj)
-classificador(obj)
+
+
+console.log(classificador(obj))
